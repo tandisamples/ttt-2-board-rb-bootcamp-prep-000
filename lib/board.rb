f55d3f1152bb7a7213f08f39ace4abec91016082
Tandi Samples <tandisamples@gmail.com>
2019-07-24 00:37:15 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,27 +1,3 @@
 # Define the variable board below
-def board
-row = [" ", " ", " "]
-# Position 1: Left 
-row[0] 
+puts board
 
-# Position 2: Middle
-row[1]
-
-# Position 3: Right   
-row[2]
-
-row = [" ", " ", " "]
-
-row[3]
-
-row[4]
-
-row[5]
-
-row = [" ", " ", " "]
-
-row[6]
-
-row[7]
-
-row[8]
